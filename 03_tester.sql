144a309ed784d850914e753b8b277a82d94288d3
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-19 18:35:53 -0400

    Update 03_tester.sql

--- 03_tester.sql
+++ 03_tester.sql
@@ -6,6 +6,8 @@ SET ECHO ON
 SET ECHO ON
 DROP TABLE tp1Client CASCADE CONSTRAINTS
 /
+DROP TABLE tp1TestDemandeSoumission CASCADE CONSTRAINTS
+/
 CREATE TABLE tp1TestClient
 (pClient 		INTEGER 		NOT NULL,
  nomClient 		VARCHAR(20) 	NOT NULL,
@@ -28,6 +30,38 @@ SET ECHO ON
 -- 
 INSERT INTO tp1TestClient
  	VALUES(2,'Luc','Samson','(999)999-9999999999','luc.samson@email.ca')
+/
+CREATE TABLE tp1TestDemandeSoumission
+(pSoumission 		INTEGER 		NOT NULL,
+ origine 		VARCHAR(20) 	NOT NULL,
+ destination 		VARCHAR(20) 	NOT NULL,
+ dateSoumission 	DATE 	NOT NULL,
+ refrigerated 		INTEGER	NOT NULL,
+ dock 		INTEGER 	NOT NULL,
+ hazardous 	INTEGER 	NOT NULL,
+ rush 		INTEGER 		NOT NULL,
+ lenght		FLOAT 	NOT NULL,
+ width 		FLOAT  	NOT NULL,
+ height 	FLOAT  	NOT NULL,
+ quantity 	INTEGER  	NOT NULL,
+ hours 	FLOAT  	NOT NULL,
+ valeur 	INTEGER  	NOT NULL,
+ pClient 		INTEGER 		NOT NULL,
+ PRIMARY KEY 	(pSoumission),
+ FOREIGN KEY 	(pClient) REFERENCES tp1Client
+)
+/
+-- 
+-- Devrait fonctionner
+-- 
+INSERT INTO tp1TestDemandeSoumission
+ 	VALUES(000001,'Toronto','Ottawa','2019/10/18',0,0,0,0,6.0,3.0,7.0,1,1.0,200.00,1)
+/
+-- 
+-- Devrait pas fonctionner car refrigerated doit etre en format INT non String
+-- 
+INSERT INTO tp1TestDemandeSoumission
+ 	VALUES(000002,'Toronto','Ottawa','2019/10/18','Oui',0,0,0,6.0,3.0,7.0,1,1.0,200.00,1)
 /
 COMMIT
 /
