68affd1f63174455c0be4b32f8e23dc7328db4b8
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-19 18:17:08 -0400

    Update 03_tester.sql

--- 03_tester.sql
+++ 03_tester.sql
@@ -1,9 +1,35 @@
 SET ECHO ON
-CREATE OR ALTER FUNCTION GETCLIENTNAME (CLNUMBER IN INTEGER) RETURN INTEGER
-IS
-CLNUMBER INTEGER;
-BEGIN
-SELECT * INTO tp1TestClient FROM tp1Client WHERE tp1Client.nomClient = 'Junayd';
-RETURN CLNUMBER;
-END GETCLIENTNAME;
+-- Script Oracle SQL*plus de creation du schema travail pratique 1 - Modélisation et Conception d'une BD
+-- 
+-- Test de la BD
+-- 
+SET ECHO ON
+DROP TABLE tp1Client CASCADE CONSTRAINTS
+/
+CREATE TABLE tp1TestClient
+(pClient 		INTEGER 		NOT NULL,
+ nomClient 		VARCHAR(20) 	NOT NULL,
+ prenomClient 		VARCHAR(20) 	NOT NULL,
+ noTelephone 	VARCHAR(15) 	NOT NULL,
+ email 	VARCHAR(50) 	NOT NULL,
+ PRIMARY KEY 	(pClient)
+)
+/
+COMMIT
+/
+SET ECHO ON
+-- 
+-- Devrait fonctionner
+-- 
+INSERT INTO tp1TestClient
+ 	VALUES(1,'Luc','Samson','(999)999-9999','luc.samson@email.ca')
+/
+SET ECHO ON
+-- 
+-- Devrait pas fonctionner car noTelephone depasse la contrainte
+-- 
+INSERT INTO tp1TestClient
+ 	VALUES(1,'Luc','Samson','(999)999-9999999999','luc.samson@email.ca')
+/
+COMMIT
 /
