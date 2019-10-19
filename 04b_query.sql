b373d090a6083e463be7708518f825cc30f0163f
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-19 14:43:26 -0400

    Update 04b_query.sql

--- 04b_query.sql
+++ 04b_query.sql
@@ -1,7 +1,7 @@
 SET ECHO ON
 -- Écrire une requête qui liste les camions qui sont présentement en voyage
 SET ECHO ON
-SELECT tp1Compagnie.nomcompagnie, tp1camion.pcamion, tp1position.ndisponible 
+SELECT tp1Compagnie.nomcompagnie, tp1camion.pcamion, tp1position.cposition, tp1position.ndisponible 
 FROM ((tp1Compagnie
 INNER JOIN tp1camion ON tp1Compagnie.pcompagnie = tp1camion.pcompagnie)
 INNER JOIN tp1position ON tp1camion.pcamion = tp1position.pcamion)
