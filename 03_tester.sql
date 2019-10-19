a0221dc4872bc6f47dbce933ba9674dd8f658c35
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-19 13:05:43 -0400

    Update 03_tester.sql

--- 03_tester.sql
+++ 03_tester.sql
@@ -1,23 +1,38 @@
-DROP TABLE IF EXISTS Tbl_TestName
- 
-CREATE TABLE Tbl_TestName
-(Id INT , DefName VARCHAR(100))
- 
- 
-INSERT INTO Tbl_TestName 
-VALUES (1,'Apple') ,  (2,'Avocado'),  (3 , 'Carrot')
+EXEC tsqlt.NewTestClass 'Func_ProductTest'
 GO
-CREATE OR ALTER FUNCTION GetProductName (@Nm INT)
-RETURNS VARCHAR(100)
-BEGIN
-DECLARE @RetVal AS VARCHAR(100)
+CREATE OR ALTER PROCEDURE Func_ProductTest.[test GetProductName casewhen1withfaketable  Expected_Mango]
+AS
+DECLARE @Expected AS VARCHAR(100)='Mango'
+DECLARE @Actual AS VARCHAR(100)
+EXEC tSQLt.FakeTable 'Tbl_TestName'
+INSERT INTO Tbl_TestName VALUES (1,'Watermelon')
+INSERT INTO Tbl_TestName VALUES (2,'Grape')
+INSERT INTO Tbl_TestName VALUES (3,'Mango')
+SELECT @Actual = dbo.GetProductName(1)
+EXEC tSQLt.AssertEquals @Expected,@Actual
+GO
  
-SELECT  @RetVal = CASE @Nm  WHEN 1 THEN (SELECT TOP 1 DefName FROM Tbl_TestName 
-ORDER BY Id DESC)
-WHEN 2 THEN 'Tomato'
-WHEN 3 THEN 'Banana'
-ELSE
-'Not Found'
-END
-RETURN  @RetVal
-END
+CREATE OR ALTER PROCEDURE Func_ProductTest.[test GetProductName casewhen2 Expected_Tomato]
+AS
+DECLARE @Expected AS VARCHAR(100)='Tomato'
+DECLARE @Actual AS VARCHAR(100)
+SELECT @Actual = dbo.GetProductName(2)
+EXEC tSQLt.AssertEquals @Expected,@Actual
+GO
+ 
+CREATE OR ALTER PROCEDURE Func_ProductTest.[test GetProductName casewhen3 Expected_Banana]
+AS
+DECLARE @Expected AS VARCHAR(100)='Banana'
+DECLARE @Actual AS VARCHAR(100)
+SELECT @Actual = dbo.GetProductName(3)
+EXEC tSQLt.AssertEquals @Expected,@Actual
+GO
+CREATE OR ALTER PROCEDURE Func_ProductTest.[test GetProductName casewhenelse Expected_NotFound]
+AS
+DECLARE @Expected AS VARCHAR(100)='Not Found'
+DECLARE @Actual AS VARCHAR(100)
+SELECT @Actual = dbo.GetProductName(4)
+EXEC tSQLt.AssertEquals @Expected,@Actual
+GO
+ 
+EXEC tSQLt.Run 'Func_ProductTest'
