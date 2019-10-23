d63325802630282f195f61c54f2cfd2bd6e2cab9
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-10-22 21:43:16 -0400

    Update 04d_query.sql

--- 04d_query.sql
+++ 04d_query.sql
@@ -1,1 +1,10 @@
-
+SET ECHO ON
+-- Une requête qui retourne les attributs des entités E = { Tracteur, Camion, Equipement }
+SET ECHO ON
+CREATE OR REPLACE VIEW entites AS
+SELECT tp1camion.tracteur, tp1camion.pcamion, tp1camion.pcompagnie,tp1camion.lenght,tp1camion.width,tp1camion.height,tp1camion.capacity,tp1typeequipement.ctypeequipement 
+FROM tp1camion
+FULL OUTER JOIN tp1typeequipement ON tp1camion.pcamion = tp1typeequipement.pcamion;
+/
+SELECT * FROM entites
+/
